ee38c65ff7edd8e45e4b36e9645f395c01a8675d
kartikeshwar.kandpal <kartikeshwar.kandpal@free-now.com>
2024-05-27 21:18:29 +0200

    First commit

---
+++ src/test/java/com/example/practice/msscbreweryeureka/MsscBreweryEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practice.msscbreweryeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBreweryEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
